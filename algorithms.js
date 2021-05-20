5e9aedb6e1aa7b28a58f6248d55063d89aa99921
ShadeB <shadebanda2@gmail.com>
2021-05-21 00:06:20 +0200

    Add checkYuGiOh function and unit tests

--- algorithms.js
+++ algorithms.js
@@ -17,3 +17,44 @@ const convertFahrToCelsius = (parameter) => {
     return celcius.toFixed(4);
   }
 } 
+
+const checkYuGiOh = (parameter) => {
+  if (isNaN(parameter)) {
+    return `invalid parameter: "${parameter}"`
+  } else {
+    let numToCheck = parseInt(parameter, 10);
+    let arrayToReturn = [];
+
+    for (let i = 1; i <= numToCheck; i++) {
+
+      if (i % 2 >= 1 && i % 3 >= 1 && i % 5 >= 1) {
+        arrayToReturn.push(i);
+        continue;
+      } else if (i % 2 === 0 && i % 3 === 0 && i % 5 === 0) {
+        arrayToReturn.push('yu-gi-oh')
+        continue;
+      } else if (i % 2 === 0 && i % 3 === 0) {
+        arrayToReturn.push('yu-gi') 
+        continue;
+      } else if (i % 2 === 0 && i % 5 === 0) {
+        arrayToReturn.push('yu-oh') 
+        continue;
+      }
+       else if (i % 3 === 0 && i % 5 === 0) {
+        arrayToReturn.push('gi-oh')
+        continue; 
+      } else if (i % 2 === 0) {
+        arrayToReturn.push('yu');
+        continue;
+      } else if (i % 3 === 0) {
+        arrayToReturn.push('gi');
+        continue;
+      } else if (i % 5 === 0) {
+        arrayToReturn.push('oh');
+        continue;
+      }
+    }
+
+    return arrayToReturn;
+  } 
+} 
--- test/spec/algorithmsSpec.js
+++ test/spec/algorithmsSpec.js
@@ -55,3 +55,37 @@ describe( 'Celcius converter', () => {
     expect(convertFahrToCelsius(parameter)).toEqual(expectedResult.toFixed(4));    
   });
 });
+
+describe('Check Yu Gi Oh', () => {
+
+  let parameter;
+  let expectedResult;
+
+  it('should return a message if the passed parameter is not a valid number', () => {
+    parameter = 'fizzbuzz is meh';
+    expectedResult = `invalid parameter: "${parameter}"`
+
+    expect(checkYuGiOh(parameter)).toEqual(expectedResult);
+  });
+
+  it('checkYuGiOh("5") should return [1, "yu", "gi", "yu", "oh"]', () => {
+    parameter = "5";
+    expectedResult = [1, "yu", "gi", "yu", "oh"];
+
+    expect(checkYuGiOh(parameter)).toEqual(expectedResult);
+  });
+
+  it('checkYuGiOh(10) should return [1, "yu", "gi", "yu", "oh", "yu-gi", 7, "yu", "gi", "yu-oh"]', () => {
+    parameter = 10;
+    expectedResult = [1, "yu", "gi", "yu", "oh", "yu-gi", 7, "yu", "gi", "yu-oh"];
+
+    expect(checkYuGiOh(parameter)).toEqual(expectedResult);
+  });
+
+  it('checkYuGiOh(30) should return [1, "yu", "gi", "yu", "oh", "yu-gi", 7, "yu", "gi", "yu-oh", 11, "yu-gi", 13, "yu", "gi-oh", "yu", 17, "yu-gi", 19, "yu-oh", "gi", "yu", 23, "yu-gi", "oh", "yu", "gi", "yu", 29, "yu-gi-oh"]', () => {
+    parameter = 30;
+    expectedResult = [1, 'yu', 'gi', 'yu', 'oh', 'yu-gi', 7, 'yu', 'gi', 'yu-oh', 11, 'yu-gi', 13, 'yu', 'gi-oh', 'yu', 17, 'yu-gi', 19, 'yu-oh', 'gi', 'yu', 23, 'yu-gi', 'oh', 'yu', 'gi', 'yu', 29, 'yu-gi-oh'];
+
+    expect(checkYuGiOh(parameter)).toEqual(expectedResult);
+  });
+});
